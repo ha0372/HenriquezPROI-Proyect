e5f179af22550e1fa17523d430e06185a6111081
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-02-12 18:11:29 -0600

    negocio

--- CHenriquez01/CHenriquez01/NEGOCIO/ClsLogin.cs
+++ CHenriquez01/CHenriquez01/NEGOCIO/ClsLogin.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CHenriquez01.DOMINIO;
 
 namespace CHenriquez01.NEGOCIO
 {
     class ClsLogin
     {
+        Login log = new Login();
+
+        public int acceso()
+        {
+            int estado = 0;
+            if (log.User.Equals("william") && log.Password.Equals("123"))
+                estado = 1;
+            {
+
+            }
+            return estado;
+        }
+
     }
 }
