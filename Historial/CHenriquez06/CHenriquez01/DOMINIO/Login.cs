e5e5f574a56a078c5325cae6a529aab5e38154b2
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-04-08 12:47:49 -0600

    actualizacion

---
+++ Historial/CHenriquez06/CHenriquez01/DOMINIO/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHenriquez01.DOMINIO
+{
+    class Login
+    {
+        private String user;
+        private String password;
+
+        public string User { get => user; set => user = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
